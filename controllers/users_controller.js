c2ec6339ee382f1e1c06373929eed096c15d5d6f
Vanshaj Gupta <vanshaj444@gmail.com>
2021-12-08 15:19:45 +0530

    sign-in and sign-up done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,67 @@
+const User=require('../models/user')
+
 module.exports.profile=function(req,res){
-    res.end('<h1>user profile</h1>')
+    return res.render('profile',{
+        title: "users-profile"
+    })
+}
+
+module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    })
+}
+
+
+
+module.exports.signIn = function(req, res){
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    })
+}
+
+
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+
+        console.log('chutiya')
+     
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log(err); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log(err); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
+
+
+module.exports.createSession = function(req, res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+
+
+    return res.redirect('/');
 }
